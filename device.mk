c78a885d64582e91b95752d0f4efb1cd5848cd54
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2013-01-25 14:32:07 +0000

    Add camera permissions
    Notify camera presence in hardware properties. Some apps filter for
    this before allowing the camera to work
    
    Change-Id: If80f63391ed1d26434fd73106bffd0fd3588b80c

--- device.mk
+++ device.mk
@@ -220,7 +220,10 @@ PRODUCT_COPY_FILES += \
 	frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
 	frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
 	frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
-	packages/wallpapers/LivePicker/android.software.live_wallpaper.xml:system/etc/permissions/android.software.live_wallpaper.xml
+	packages/wallpapers/LivePicker/android.software.live_wallpaper.xml:system/etc/permissions/android.software.live_wallpaper.xml \
+	frameworks/native/data/etc/android.hardware.camera.autofocus.xml:system/etc/permissions/android.hardware.camera.autofocus.xml \
+	frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml
+
 
 # The OpenGL ES API level that is natively supported by this device.
 # This is a 16.16 fixed point number
